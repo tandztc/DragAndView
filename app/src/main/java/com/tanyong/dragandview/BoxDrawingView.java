88a25fcddc27a1da6163a6aa126040df248f723f
tandztc <tandztc@yeah.net>
2016-03-02 15:19:36 +0800

    重写触摸事件和draw方法

--- app/src/main/java/com/tanyong/dragandview/BoxDrawingView.java
+++ app/src/main/java/com/tanyong/dragandview/BoxDrawingView.java
@@ -1,7 +1,13 @@
 package com.tanyong.dragandview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,11 +15,46 @@ import android.view.View;
  * 自定义视图类
  */
 public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
+    private Paint mBackgroundPaint;
+
     public BoxDrawingView(Context context) {
         this(context, null);
     }
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(Color.GRAY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
     }
 }
